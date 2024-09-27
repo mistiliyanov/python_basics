e8b7e969d5d8400b0d789084d51705dbf1c2947d
mistiliyanov <mistiliyanov@salesforce.com>
2024-09-27 17:46:42 +0100

    packages

---
+++ training_topics/13. Modules and Packages/greetings.py
@@ -0,0 +1,3 @@
+# greetings.py
+def say_hello(name):
+    return f"Hello, {name}!"
---
+++ training_topics/13. Modules and Packages/math_utils.py
@@ -0,0 +1,17 @@
+# sum
+def add_numbers(a, b):
+    return a + b
+
+# subtract
+def subtract_numbers(a, b):
+    return a - b
+
+# multiply
+def multiply_numbers(a, b):
+    return a * b
+
+# divide
+def divide_numbers(a, b):
+    if b == 0:
+        return 'div/0'
+    return a / b
